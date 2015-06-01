96a9cf594cf1f41bf1c03787d80942ed4db3ab3f
Jose Miguel Delgado <jmg4delgado@gmail.com>
2015-06-01 22:57:31 +0200

    Ejercicio obligatorio Modulo 6
    Cambios pedidos en el ejercicio obligatorio del Modulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,3 +12,7 @@
        res.render('quizes/answer', {respuesta: 'Incorrecto'});
     }
  };
+
+ exports.author = function(req, res) {
+    res.render('author', {autor: 'Jose Miguel Delgado'});
+ };
