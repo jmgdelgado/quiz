f2e9d97b24f53cbb8559d2a99c47c5df41702e35
Jose Miguel Delgado <jmg4delgado@gmail.com>
2015-06-07 15:37:07 +0200

    Editar pregunta
    Boton para editar las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,7 +47,7 @@ exports.index = function (req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
   res.render('quizes/show', { quiz: req.quiz, errors : []});
-};
+};          // req.quiz: instancia de quiz cargada con autoload
 
 
 // GET /quizes/:id/answer
@@ -116,3 +116,55 @@ exports.create = function(req, res){
   .then( function(){ res.redirect('/quizes')}) ;
  }
 };
+
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  var errors = req.quiz.validate();//ya qe el objeto errors no tiene then(
+
+  if (errors)
+      {
+        var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+        for (var prop in errors) errores[i++]={message: errors[prop]};
+        res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+
+ };
+
+/*
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
+ };
+*/
