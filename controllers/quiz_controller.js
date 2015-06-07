b7cd13e29d0e6740cea2d2776229906d6fb0085c
Jose Miguel Delgado <jmg4delgado@gmail.com>
2015-06-07 15:13:35 +0200

    Validar crear preguntas
    Validacion del formulario de creacion de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,6 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
+// Autoload :id
 exports.load = function(req, res, next, quizId) {
  models.Quiz.find(quizId).then(
    function(quiz) {
@@ -18,7 +18,7 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
   models.Quiz.findAll().then(
     function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
+      res.render('quizes/index', { quizes: quizes, errors: []});
     }
   ).catch(function(error) { next(error);})
 };
@@ -37,7 +37,7 @@ exports.index = function (req, res) {
     }
   ).then(
     function(quizes) {
-      res.render('quizes/index' , { quizes : quizes });
+      res.render('quizes/index' , { quizes : quizes, errors : [] });
     }
   ).catch(function(error) { next(error);});
 }
@@ -46,7 +46,7 @@ exports.index = function (req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors : []});
 };
 
 
@@ -56,33 +56,63 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors : []});
 };
 
 
 exports.author = function(req, res) {
-   res.render('author', {autor: 'Jose Miguel Delgado'});
+   res.render('author', {autor: 'Jose Miguel Delgado', errors : []});
 };
 
 
 // GET /quizes/new
 exports.new = function(req, res) {
-  var quiz = models.Quiz.build(
+  var quiz = models.Quiz.build( // crea objeto quiz
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors : []});
 };
 
 
 // POST /quizes/create
+/*
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
 
 // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })   // res.redirect: Redirección HTTP a lista de preguntas
-}; 
+   quiz
+   .validate()
+   .then(
+     function(err){
+       if (err) {
+         res.render('quizes/new', {quiz: quiz, errors: err.errors});
+       } else {
+         quiz // save: guarda en DB campos pregunta y respuesta de quiz
+         .save({fields: ["pregunta", "respuesta"]})
+         .then( function(){ res.redirect('/quizes')})
+       }      // res.redirect: Redirección HTTP a lista de preguntas
+     }
+   );
+ };
+*/
+
+
+
+exports.create = function(req, res){
+ var quiz = models.Quiz.build( req.body.quiz );
+
+ var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+ if (errors)
+ {
+  var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+  for (var prop in errors) errores[i++]={message: errors[prop]};
+    res.render('quizes/new', {quiz: quiz, errors: errores});
+ } else {
+  quiz // save: guarda en DB campos pregunta y respuesta de quiz
+  .save({fields: ["pregunta", "respuesta"]})
+  .then( function(){ res.redirect('/quizes')}) ;
+ }
+};
